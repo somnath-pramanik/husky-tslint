484d2155009e2b5d4634fcf19d8f9121dca2f631
Somnath Pramanik <somnath@thethinkerz.com>
2021-11-26 04:29:27 +0530

    com

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'husky-tslint';
+
+  private Add(){
+    const a = "Hello";
+  }
+}
